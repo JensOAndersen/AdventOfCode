a7795fa0e66d5cdc4cdfd625dd2b77ebaf103eb7
Jens <jensoleand@gmail.com>
2021-12-13 15:47:31 +0100

    solved day 2 part 2

--- AoC.Solutions/Y2021/Day2 .cs
+++ AoC.Solutions/Y2021/Day2 .cs
@@ -14,7 +14,7 @@ namespace AoC.Solutions.Y2021
 
         public Day2(ResultParsers res) : base(res.IntToString, res.IntToString)
         {
-            
+
         }
 
         public override int Day => 2;
@@ -32,7 +32,7 @@ namespace AoC.Solutions.Y2021
                     return new DirDto
                     {
                         Direction = dir,
-                        Dist = length
+                        Value = length
                     };
                 else
                     throw new ArgumentException("could not parse line: " + line);
@@ -52,13 +52,13 @@ namespace AoC.Solutions.Y2021
                 switch (current.Direction)
                 {
                     case Directions.Forward:
-                        state[x] += current.Dist;
+                        state[x] += current.Value;
                         break;
                     case Directions.Up:
-                        state[y] -= current.Dist;
+                        state[y] -= current.Value;
                         break;
                     case Directions.Down:
-                        state[y] += current.Dist;
+                        state[y] += current.Value;
                         break;
                     default:
                         break;
@@ -72,7 +72,39 @@ namespace AoC.Solutions.Y2021
 
             return xVal * yVal;
         }
+        protected override int PartTwoImplementation()
+        {
+            const string aim = "aim";
+            const string x = "x";
+            const string y = "y";
 
+            var summed = _data.Aggregate(new Dictionary<string, int>()
+            {
+                {x, 0 },
+                {y,0 },
+                {aim, 0 }
+            }, (state, current) =>
+            {
+                switch (current.Direction)
+                {
+                    case Directions.Forward:
+                        state[x] += current.Value;
+                        state[y] += current.Value * state[aim];
+                        break;
+                    case Directions.Up:
+                        state[aim] -= current.Value;
+                        break;
+                    case Directions.Down:
+                        state[aim] += current.Value;
+                        break;
+                }
+
+                return state;
+            });
+
+            return summed[y] * summed[x];
+
+        }
         private enum Directions
         {
             Forward,
@@ -83,7 +115,7 @@ namespace AoC.Solutions.Y2021
         private class DirDto
         {
             public Directions Direction { get; set; }
-            public int Dist { get; set; }
+            public int Value { get; set; }
         }
     }
 }
