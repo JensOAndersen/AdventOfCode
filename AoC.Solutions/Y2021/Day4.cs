b9c5b573f67e48890e86a4cb79cea31c541e90f2
Jens <jensoleand@gmail.com>
2021-12-14 19:12:36 +0100

    tried solving day 4 part 2 - unsuccessfully

--- AoC.Solutions/Y2021/Day4.cs
+++ AoC.Solutions/Y2021/Day4.cs
@@ -8,6 +8,12 @@ using System.Threading.Tasks;
 
 namespace AoC.Solutions.Y2021
 {
+    /*
+    I found this to be an interesting task for structuring date.
+
+    My idea for solving it, was to organize the data into classes which each have a dictionary of what values goes into which rows and colums,
+    then having a 'hit point' method to track which rows have a certain amount of hits
+     */
     public class Day4 : SolutionBase<int, int>
     {
         private string[] _lineData;
@@ -27,33 +33,11 @@ namespace AoC.Solutions.Y2021
 
         protected override int PartOneImplementation()
         {
-            var man = new CheckerBoardManager();
-            var instructions = "";
-            var rowNum = 0;
-            CheckerBoard currentBoard = null;
-            for (int i = 0; i < _lineData.Length; i++)
-            {
-                if(i == 0)
-                {
-                    instructions = _lineData[i];
-                    continue;
-                }
+            var pair = BuildManagerAndInstructions();
 
-                if (string.IsNullOrWhiteSpace(_lineData[i]))
-                {
-                    currentBoard = new CheckerBoard();
-                    rowNum = 0;
-                    continue;
-                }
-                else {
-                    currentBoard.FeedLine(man, _lineData[i]);
-                    rowNum++;
-                }
-            }
-
-            foreach (var instruction in instructions.Split(','))
+            foreach (var instruction in pair.instructions.Split(','))
             {
-                if(man.FeedInstruction(instruction, out var points))
+                if (pair.manager.FeedInstruction(instruction, out var points))
                 {
                     return int.Parse(instruction) * points.Select(p => int.Parse(p)).Sum();
                 }
@@ -64,32 +48,97 @@ namespace AoC.Solutions.Y2021
 
         protected override int PartTwoImplementation()
         {
-            return base.PartTwoImplementation();
+            var pair = BuildManagerAndInstructions();
+
+            var lastCompleted = new string[0];
+            var lastInstruction = "";
+            var instructHashSet = pair.instructions.Split(',').Select(x => int.Parse(x)).ToHashSet();
+
+            foreach (var instruction in pair.instructions.Split(','))
+            {
+                lastInstruction = instruction;
+                if (pair.manager.FeedInstruction(instruction, out var points))
+                {
+                    if (pair.manager.RemainingBoards == 0)
+                    {
+                        lastCompleted = points;
+                        break;
+                    }
+                }
+            }
+
+            return int.Parse(lastInstruction) * lastCompleted.Select(p => int.Parse(p)).Sum();
+        }
+
+        private (CheckerBoardManager manager, string instructions) BuildManagerAndInstructions()
+        {
+            var man = new CheckerBoardManager();
+            var instructions = "";
+            var rowNum = 0;
+            CheckerBoard currentBoard = null;
+
+            for (int i = 0; i < _lineData.Length; i++)
+            {
+                if (i == 0)
+                {
+                    instructions = _lineData[i];
+                    continue;
+                }
+
+                if (string.IsNullOrWhiteSpace(_lineData[i]))
+                {
+                    currentBoard = new CheckerBoard(man);
+                    rowNum = 0;
+                    continue;
+                }
+                else
+                {
+                    currentBoard.FeedLine(_lineData[i]);
+                    rowNum++;
+                }
+            }
+
+            return (man, instructions);
         }
 
         class CheckerBoardManager
         {
             private Dictionary<string, List<CheckerBoard>> _boards = new Dictionary<string, List<CheckerBoard>>();
-
+            private Dictionary<CheckerBoard, bool> _allBoards = new Dictionary<CheckerBoard, bool>();
+            public int RemainingBoards => _allBoards.Values.Where(x => !x).Count();
             internal bool FeedInstruction(string instruction, out string[] unhitPoints)
             {
-                if(_boards.TryGetValue(instruction, out var boardsWithInstruction))
+                string[] aHit = null;
+                var didHit = false;
+                if (_boards.TryGetValue(instruction, out var boardsWithInstruction))
                 {
                     foreach (var board in boardsWithInstruction)
                     {
                         if (board.HitPoint(instruction, out var unhitPoint))
                         {
-                            unhitPoints = unhitPoint;
-                            return true;
+                            _allBoards[board] = true;
+
+                            if (didHit)
+                                continue;
+
+                            aHit = unhitPoint;
+                            didHit = true;
                         }
                     }
                 }
-                unhitPoints = default;
-                return false;
+                if (!didHit)
+                    unhitPoints = default;
+                else
+                    unhitPoints = aHit;
+
+                return didHit;
             }
 
             internal void RegisterItem(string item, CheckerBoard checkerBoard)
             {
+                if (!_allBoards.ContainsKey(checkerBoard))
+                    _allBoards.Add(checkerBoard,false);
+
                 if (!_boards.TryGetValue(item, out var boards))
                 {
                     boards = new List<CheckerBoard>();
@@ -98,6 +147,8 @@ namespace AoC.Solutions.Y2021
                 }
                 else
                     boards.Add(checkerBoard);
+
+                checkerBoard.RegisterContainer(boards);
             }
         }
 
@@ -105,12 +156,20 @@ namespace AoC.Solutions.Y2021
         {
 
             private Dictionary<string, int> _columns = new Dictionary<string, int>();
-            private Dictionary<string, int> _rows= new Dictionary<string, int>();
+            private Dictionary<string, int> _rows = new Dictionary<string, int>();
 
             private Dictionary<int, int> _hitRows = new Dictionary<int, int>();
             private Dictionary<int, int> _hitColumns = new Dictionary<int, int>();
 
-            public void FeedLine(CheckerBoardManager manager, string line)
+            private List<List<CheckerBoard>> _registeredIn = new List<List<CheckerBoard>>();
+            private CheckerBoardManager _manager;
+
+            public CheckerBoard(CheckerBoardManager manager)
+            {
+                _manager = manager;
+            }
+
+            public void FeedLine(string line)
             {
                 var nextRow = _rows.Any() ? _rows.Max(x => x.Value) + 1 : 0;
                 int iterator = 0;
@@ -118,14 +177,14 @@ namespace AoC.Solutions.Y2021
                 {
                     if (string.IsNullOrWhiteSpace(item))
                         continue;
-                    
+
                     if (!_columns.TryAdd(item, iterator))
                         throw new ArgumentException("column already contains value");
 
-                    if(!_rows.TryAdd(item, nextRow))
+                    if (!_rows.TryAdd(item, nextRow))
                         throw new ArgumentException("Row already contains value");
 
-                    manager.RegisterItem(item, this);
+                    _manager.RegisterItem(item, this);
 
                     iterator++;
                 }
@@ -133,40 +192,47 @@ namespace AoC.Solutions.Y2021
 
             public bool HitPoint(string point, out string[] unhitPoints)
             {
-                if(_columns.TryGetValue(point, out var result))
-                {
-                    _columns.Remove(point);
-                    if (_hitColumns.ContainsKey(result))
-                    {
-                        _hitColumns[result]++;
-                        if(_hitColumns[result] == 5)
-                        {
-                            unhitPoints = _columns.Keys.ToArray();
-                            return true;
-                        }
-                    }
-                    else
-                        _hitColumns.Add(result, 1);
-                }
+                var wasHitCol = DoHit(_columns, _hitColumns, point, out var unhitPointsCol);
+                var wasHitRow = DoHit(_rows, _hitRows, point, out var unhitPointsRow);
 
-                if(_rows.TryGetValue(point, out result))
-                {
-                    _rows.Remove(point);
-                    if (_hitRows.ContainsKey(result))
-                    {
-                        _hitRows[result]++;
-                        if (_hitRows[result] == 5)
-                        {
-                            unhitPoints = _rows.Keys.ToArray();
-                            return true;
-                        }
-                    }
-                    else
-                        _hitRows.Add(result, 1);
-                }
+                if (wasHitCol && !wasHitRow)
+                    unhitPoints = unhitPointsCol;
+                else if (!wasHitCol && wasHitRow)
+                    unhitPoints = unhitPointsRow;
+                else if (wasHitCol && wasHitRow)
+                    unhitPoints = unhitPointsRow;
+                else
+                    unhitPoints = default;
 
-                unhitPoints = new string[0];
-                return false;
+                return wasHitCol || wasHitRow;
+
+                bool DoHit(Dictionary<string, int> hitIn, Dictionary<int, int> hitCounter, string point, out string[] unhitElements)
+                {
+                    if (hitIn.TryGetValue(point, out var result))
+                    {
+                        hitIn.Remove(point);
+                        if (hitCounter.ContainsKey(result))
+                        {
+                            hitCounter[result]++;
+                            if (hitCounter[result] == 5)
+                            {
+                                unhitElements = hitIn.Keys.ToArray();
+                                return true;
+                            }
+                        }
+                        else if (!hitCounter.ContainsKey(result))
+                            hitCounter.Add(result, 1);
+                    }
+                    unhitElements = default;
+                    return false;
+                }
+            }
+
+
+            internal void RegisterContainer(List<CheckerBoard> boards)
+            {
+                if (!_registeredIn.Contains(boards))
+                    _registeredIn.Add(boards);
             }
         }
     }
