415fde18c8796ff79a777d1acee22e35f1e45ec4
Jens <jensoleand@gmail.com>
2021-12-16 19:14:48 +0100

    Added solution for day 5 - however part 2 gives wrong output with full input, correct with sample

---
+++ AoC.Library/Models/Point.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AoC.Library.Models
+{
+    public struct Point : IEquatable<Point>
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point(string couple, string delimiter)
+        {
+            var split = couple.Split(delimiter);
+
+            X = int.Parse(split[0]);
+            Y = int.Parse(split[1]);
+        }
+
+        public int X { get; }
+        public int Y { get; }
+
+        public bool Equals(Point other) => other.X == X && other.Y == Y;
+
+        public Point MoveCloserTo(Point to) => new Point(GetOneCloser(X,to.X), GetOneCloser(Y, to.Y));
+
+        private int GetOneCloser(int from, int to)
+        {
+            if (from < to)
+                return from + 1;
+            else if (from > to)
+                return from - 1;
+            else
+                return from;
+        }
+    }
+}
---
+++ AoC.Solutions/Y2021/Day5.cs
@@ -0,0 +1,124 @@
+﻿using AoC.Library.Abstractions;
+using AoC.Library.Helpers;
+using AoC.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.Solutions.Y2021
+{
+    public partial class Day5 : SolutionBase<int, int>
+    {
+        private IEnumerable<Day5Dto> _data;
+
+        public Day5(ResultParsers resPar) : base(resPar.IntToString, resPar.IntToString)
+        {
+        }
+
+        public override int Day => 5;
+
+        public override int Year => 2021;
+
+        public override async Task Init()
+        {
+            _data = await InputSource.GetInputForDay(Day, str => str.Trim().Split("\n").Select(line => new Day5Dto(line)));
+        }
+
+        protected override int PartOneImplementation()
+        {
+            var result = _data.Where(dt => dt.IsStraight()).Aggregate(new Dictionary<Point, int>(), (state, current) =>
+            {
+                if (!current.IsStraight())
+                    return state;
+
+                foreach (var point in GetPointsBetweenPointsFrom(current))
+                {
+                    if (state.ContainsKey(point))
+                        state[point]++;
+                    else
+                        state.Add(point, 1);
+                }
+
+                return state;
+            });
+
+            return result.Values.Where(x => x > 1).Count();
+        }
+
+        //this gives the wrong output with full input, correct with sample input
+        protected override int PartTwoImplementation()
+        {
+            var result = _data.Aggregate(new Dictionary<Point, int>(), (state, current) =>
+            {
+                foreach (var point in GetPointsBetweenPointsFrom(current).ToArray())
+                {
+                    if (state.ContainsKey(point))
+                        state[point]++;
+                    else
+                        state.Add(point, 1);
+                }
+
+                return state;
+            });
+
+            return result.Values.Where(x => x > 1).Count();
+        }
+
+        private IEnumerable<Point> GetPointsBetweenPointsFrom(Day5Dto dto)
+        {
+            var from = dto.From;
+            var to = dto.To;
+
+            yield return from;
+
+            while (!from.Equals(to))
+                yield return from = from.MoveCloserTo(to);
+        }
+
+        //original part one solution
+        private IEnumerable<Point> GetStraightPointsBetweenPointsFrom(Day5Dto cto)
+        {
+            if (!cto.IsStraight())
+                throw new ArgumentException();
+
+            if (cto.IsVertical())
+            {
+                var lowestY = cto.From.Y < cto.To.Y ? cto.From.Y : cto.To.Y;
+                var highestY = cto.From.Y > cto.To.Y ? cto.From.Y : cto.To.Y;
+
+                for (int i = lowestY; i <= highestY; i++)
+                    yield return new Point(cto.From.X, i);
+
+            }
+            else
+            {
+                var lowestX = cto.From.X < cto.To.X ? cto.From.X : cto.To.X;
+                var highestX = cto.From.X > cto.To.X ? cto.From.X : cto.To.X;
+
+                for (int i = lowestX; i <= highestX; i++)
+                    yield return new Point(i, cto.From.Y);
+
+            }
+        }
+
+        class Day5Dto
+        {
+            public Point From { get; }
+            public Point To { get; }
+            public Day5Dto(string line)
+            {
+                var fromTo = line.Split(" -> ");
+
+                From = new Point(fromTo[0], ",");
+                To = new Point(fromTo[1], ",");
+            }
+
+            public bool IsVertical() => From.X == To.X;
+            public bool IsHorizontal() => From.Y == To.Y;
+            public bool IsStraight() => IsVertical() || IsHorizontal();
+        }
+    }
+}
