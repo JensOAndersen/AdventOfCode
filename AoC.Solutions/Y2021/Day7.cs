9ddf38b0a7e9f957f39fefad82f8c9c3e31f1a19
Jens <jensoleand@gmail.com>
2021-12-19 15:41:30 +0100

    Solved day 7 part 1 with brute force

---
+++ AoC.Solutions/Y2021/Day7.cs
@@ -0,0 +1,77 @@
+﻿using AoC.Library.Abstractions;
+using AoC.Library.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.Solutions.Y2021
+{
+    internal class Day7 : SolutionBase<int, int>
+    {
+        private int[] _data;
+
+        public Day7(ResultParsers resPar) : base(resPar.IntToString, resPar.IntToString)
+        {
+        }
+
+        public override int Day => 7;
+
+        public override int Year => 2021;
+
+        public override async Task Init()
+        {
+            _data = (await InputSource.GetInputForDay(Day, str => str.Trim().Split(",").Select(x => int.Parse(x)))).ToArray();
+        }
+
+        protected override int PartOneImplementation()
+        {
+            int min = _data.Min();
+            int max = _data.Max();
+
+            var resDict = new Dictionary<int, int>();
+
+            //brute force
+            for (int i = min; i < max; i++)
+                resDict.Add(i, CalculateFuelCostForPosition(i));
+
+            var rest1 = resDict.Values.Min();
+
+            //there is a value difference of two between the brute force and the recursive method....
+            //var res = CalculatePartOneRecursively(min, max, int.MaxValue);
+
+            return rest1;
+        }
+
+        //actually trying to be smart
+        private int CalculatePartOneRecursively(int min, int max, int previousMin)
+        {
+            //calculate fuel cost for min and max values
+            var minFuel = CalculateFuelCostForPosition(min);
+            var maxFuel = CalculateFuelCostForPosition(max);
+
+            //find midpoint
+            var halfPoint = (max + min) / 2;
+
+            //figure out which of the two fuel spenders use the least
+            var leastFuel = minFuel < maxFuel ? minFuel : maxFuel;
+
+            //if least is higher than previous, return previous, also if halfpoint equals min, then we're down to a difference of 1 between min and max, doing this because of integer division, otherwise it results in a stackoverflow
+            if (leastFuel > previousMin || halfPoint == min)
+                return leastFuel;
+
+            if (minFuel == maxFuel)
+                return maxFuel;
+
+            //if fuel cost for min value is less than fuel cost for max value, then we continue recursion for the lower half of the points between min and halfpoint
+            if (minFuel < maxFuel)
+                return CalculatePartOneRecursively(min, halfPoint, leastFuel);
+
+            //reverse for the other way around
+            return CalculatePartOneRecursively(halfPoint, max, leastFuel);
+        }
+
+        private int CalculateFuelCostForPosition(int pos) => _data.Select(x => Math.Abs(pos - x)).Sum();
+    }
+}
